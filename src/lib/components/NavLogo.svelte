08159e2c8d40fb083d96ee95c0d21864610722d2
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-07-03 16:17:25 -0300

    bug fix en safari con la font del logo recortada

--- src/lib/components/NavLogo.svelte
+++ src/lib/components/NavLogo.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 </script>
 
-<a href="/" class="flex h-full w-full items-center gap-2 py-3 text-3xl">
+<a href="/" class="flex h-full w-full items-center gap-1 py-3 text-3xl">
 	<img
 		src="icons/logo-ana.svg"
 		alt="logo"
 		class="h-full object-contain object-left drop-shadow-md"
 	/>
-	<span class="font-quicksand font-bold tracking-tight text-gray-700 drop-shadow-lg">cepia</span>
+	<div
+		class="font-quicksand min-w-[90px] overflow-visible text-center font-bold tracking-tight text-gray-700 drop-shadow-lg"
+	>
+		cepia
+	</div>
 </a>
