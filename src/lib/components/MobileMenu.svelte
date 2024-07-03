2c6049e06806c86da2edd36e6bd2469d95dd837f
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-07-03 17:37:28 -0300

    better mobile menu

--- src/lib/components/MobileMenu.svelte
+++ src/lib/components/MobileMenu.svelte
@@ -13,17 +13,20 @@
 <Sheet.Root bind:open={isOpen}>
 	<Sheet.Trigger class={buttonClass}><MenuIcon /></Sheet.Trigger>
 	<Sheet.Content class="max-w-[250px]">
-		<ul class="flex flex-col gap-4 px-6 text-lg">
+		<ul class="mt-8 flex flex-col gap-4 px-6 text-lg">
 			{#each links as { name, href }, i}
 				<li>
-					<a
-						{href}
-						on:click={() => (isOpen = false)}
-						class:active-nav-link={href === $page.url.pathname}
-						class="active:text-gray-300">{name}</a
-					>
+					<a {href} on:click={() => (isOpen = false)} aria-current={href === $page.url.pathname}
+						>{name}
+					</a>
 				</li>
 			{/each}
 		</ul>
 	</Sheet.Content>
 </Sheet.Root>
+
+<style>
+	li a[aria-current='true'] {
+		@apply border-b-2 border-gray-600;
+	}
+</style>
