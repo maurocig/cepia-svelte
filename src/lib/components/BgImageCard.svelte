aba599ab9a32f8b73f94c1bdf71e170aecef2f63
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-07-03 17:18:32 -0300

    text in services is hidden in mobile until hover/tap

--- src/lib/components/BgImageCard.svelte
+++ src/lib/components/BgImageCard.svelte
@@ -17,7 +17,7 @@
 		className="h-full md:from-white/70 md:backdrop-blur-0 md:bg-gradient-to-b md:to-emerald-200/50  hover:bg-white/20 md:via-white/30 duration-700 border-slate-900/20 hover:border-slate-900/60 hover:bg-opacity-0 hover:backdrop-blur-xl transition group"
 	>
 		<p
-			class="group text-[17px] text-lg text-slate-800 drop-shadow-sm transition duration-700 group-hover:opacity-100 md:opacity-0"
+			class="group text-[17px] text-lg text-slate-800 opacity-0 drop-shadow-sm transition duration-700 group-hover:opacity-100"
 		>
 			{description}
 		</p>
