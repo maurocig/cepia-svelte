be1e69563c03b79f2021e1f047f45da0ac98ca02
Mauro Cigliuti <mcigliuti01@gmail.com>
2024-07-03 17:53:18 -0300

    added target _top so it navigates to the top of the page when link clicked from mobile menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -18,6 +18,7 @@
 			{#each links as { name, href }, i}
 				<li>
 					<a
+						target="_top"
 						class="drop-shadow-sm transition hover:opacity-70"
 						{href}
 						aria-current={href === $page.url.pathname}>{name}</a
